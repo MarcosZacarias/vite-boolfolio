d9045e8867a0a56059bcc6486507c30769a1d0b9
MarcosZacarias <marcoszacariasdaniel@gmail.com>
2023-11-03 15:47:56 +0100

    add: setup files

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<template>
+  <h1>Hello Word</h1>
+</template>
+
+<style lang="scss">
+h1 {
+  color: red;
+}
+</style>
